32cf53e6505ea4c051c078723016c92711829e60
Z0ck0 <132205377+Z0ck0@users.noreply.github.com>
2026-01-21 15:04:55 +0100

    add env credentials strategy with dotenv
    dded a local .env.credentials file for Cypress login data and a .env.credentials.example template. Updated .gitignore to keep real secrets out of Git while still tracking the template, and finally configured cypress.config.ts to load credentials from the custom path and expose them via Cypress.env().”

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,7 +3,13 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   e2e: {
     baseUrl: "https://www.saucedemo.com/",
-    
+    env: {
+      PASSWORD: process.env.PASSWORD,
+      STANDARD_USER: process.env.STANDARD_USER,
+      LOCKED_OUT_USER: process.env.LOCKED_OUT_USER,
+      PROBLEM_USER: process.env.PROBLEM_USER,
+      ERROR_USER: process.env.ERROR_USER,
+    },    
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
