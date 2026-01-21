b041c2320d9449470f69b49b307cfd209f45db95
Z0ck0 <132205377+Z0ck0@users.noreply.github.com>
2026-01-21 15:10:35 +0100

    add UserRole enum for login credentials

---
+++ cypress/support/enums/userRole.ts
@@ -0,0 +1,6 @@
+export const UserRole = {
+  SAUCE_STANDARD_USER: "standard_user",
+  SAUCE_LOCKED_OUT_USER: "locked_out_user",
+  SAUCE_PROBLEM_USER: "problem_user",
+  SAUCE_ERROR_USER: "error_user",
+};
